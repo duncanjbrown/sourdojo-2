5a7e4d0539b0fc5b1c70805ef7291e5f15199916
Duncan Brown <duncanjbrown@gmail.com>
2020-03-05 23:00:41 +0000

    Complete port to re-frame

--- src/sourdojo/core.cljs
+++ src/sourdojo/core.cljs
@@ -5,21 +5,9 @@
             [sourdojo.events]
             [sourdojo.components.add-note :as add-note]
             [sourdojo.components.add-photo :as add-photo]
-            ;; [sourdojo.components.step-buttons :as step-buttons]
             [sourdojo.components.current-state :as current-state]
             [sourdojo.components.step-buttons :as step-buttons]
             [sourdojo.components.timeline :as timeline]))
-
-;; (defn current-bake []
-;;   (let [state (r/cursor app-state [:state])
-;;         events (r/cursor app-state [:events])]
-;;     [:div
-;;       [state-display/render @state]
-;;       [step-buttons/render state events]
-;;       [add-note/render events]
-;;       [add-photo/render events]
-;;       [timeline/render @events]
-;;       [:a {:href (rfe/href ::about)} "About"]]))
 
 (defn app []
   [:div
