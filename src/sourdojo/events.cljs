1b75012dfd973bab3dc7db61c6d9f8ccbf49d7e2
Duncan Brown <duncanjbrown@gmail.com>
2020-04-12 21:15:23 +0100

    Add an interceptor to sync actions to Firestore on save
    My First Interceptor

--- src/sourdojo/events.cljs
+++ src/sourdojo/events.cljs
@@ -5,6 +5,18 @@
    [sourdojo.bake-state-machine :as bake-states]
    [re-frame.core :refer [reg-event-db reg-event-fx]]))
 
+(def save-to-firestore
+  (re-frame.core/->interceptor
+   :id      :save-to-firestore
+   :after   (fn [context]
+              (let [new-bake-value (get-in context [:effects :db :current-bake])]
+                (assoc-in context [:effects :dispatch] [:save-to-firestore new-bake-value])))))
+
+(reg-event-fx
+ :save-to-firestore
+ (fn [_ [_ bake]]
+   (println bake)))
+
 (reg-event-fx
   :signed-in
   (fn [{:keys [db]} [_ user]]
@@ -22,11 +34,13 @@
 
 (reg-event-fx
  :initialise-bake
+ [save-to-firestore]
  (fn [{:keys [db]} _]
    {:db (assoc db :current-bake bake/new-bake)}))
 
 (reg-event-fx
  :transition!
+ [save-to-firestore]
  (fn [{:keys [db]} [_ new-state step]]
    {:db (->
          db
@@ -35,10 +49,12 @@
 
 (reg-event-fx
  :add-note
+ [save-to-firestore]
  (fn [{:keys [db]} [_ note]]
    {:db (update-in db [:current-bake :steps] conj note)}))
 
 (reg-event-fx
  :add-photo
+ [save-to-firestore]
  (fn [{:keys [db]} [_ photo]]
    {:db (update-in db [:current-bake :steps] conj photo)}))
