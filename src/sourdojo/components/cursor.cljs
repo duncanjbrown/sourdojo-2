94189b8a4e72da541003d3964039209b6ac1353a
Duncan Brown <duncanjbrown@gmail.com>
2020-04-20 22:58:01 +0100

    Put the UI into a cursor which follows the timeline

---
+++ src/sourdojo/components/cursor.cljs
@@ -0,0 +1,21 @@
+(ns sourdojo.components.cursor
+  (:require [re-frame.core :as rf]
+            [sourdojo.components.step-buttons :as step-buttons]
+            [sourdojo.components.add-note :as add-note]
+            [sourdojo.components.add-photo :as add-photo]))
+
+(defn- starting-cursor []
+  [:h1
+   [:a {:href "#" :on-click #(rf/dispatch [:transition! :new {:type :step :step :start :time (js/Date.)}])} "start!"]])
+
+(defn- in-progress-cursor []
+  [:div
+    [step-buttons/render @(rf/subscribe [:current-state])]
+    [add-note/render]
+    [add-photo/render]])
+
+(defn render [current-state]
+  [:div.cursor
+   (if (= :origin current-state)
+     (starting-cursor)
+     (in-progress-cursor))])
