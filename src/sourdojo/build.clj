e2e8bb946a5b7646d9c9db6a7295fb4c864c3891
Duncan Brown <duncanjbrown@gmail.com>
2020-05-09 23:18:20 +0100

    Render index.html from hiccup
    This makes it easy to use fingerprinted asset paths

--- src/sourdojo/build.clj
+++ src/sourdojo/build.clj
@@ -1,6 +1,8 @@
 (ns sourdojo.build
   (:require [aero.core :as aero]
             [sourdojo.env :as env]
+            [clojure.edn :as edn]
+            [hiccup.page :refer [html5]]
             [shadow.cljs.devtools.config :as shadow-config]
             [shadow.cljs.devtools.api :as shadow]))
 
@@ -13,20 +15,43 @@
 ;; these are to be run from the command line, with a release-flag parameter:
 ;; $ shadow-cljs clj-run app.build/release staging
 
+(defn index-html
+  [{:keys [output-dir asset-path]}]
+  (let [main-js-path (->> (slurp (str output-dir "/manifest.edn"))
+                          (edn/read-string)
+                          (first)
+                          (:output-name)
+                          (str asset-path "/"))]
+    (html5 {:lang "en"}
+      [:head
+        [:meta {:charset "UTF-8"}]
+        [:meta {:name "viewport", :content "width=device-width, initial-scale=1"}]
+        [:link {:rel "icon", :type "image/x-icon", :href "favicon.ico"}]
+        [:link {:rel "stylesheet", :href "/style.css", :type "text/css"}]
+        [:title "Sourdojo"]]
+      [:body
+        [:noscript "You need to enable JavaScript to run this app."]
+        [:div {:id "app"}]
+        [:script {:src main-js-path, :type "text/javascript"}]])))
+
 (defn release
   ([] (release "local"))
   ([release-flag]
-   (shadow/release* (-> (shadow-config/get-build! :dist)
-                        ;; note, we add ::release-flag to our build-config, we need this later.
-                        (assoc ::release-flag release-flag)) {})))
+   (let [build-config (shadow-config/get-build! :dist)]
+    (shadow/release* (-> build-config
+                         ;; note, we add ::release-flag to our build-config, we need this later.
+                         (assoc ::release-flag release-flag)) {})
+    (spit "dist/index.html" (index-html build-config)))))
 
 (defn watch
   "Watch the :app build, reloading on changes."
   {:shadow/requires-server true}
   ([] (watch "local"))
   ([release-flag]
-   (shadow/watch (-> (shadow-config/get-build! :app)
-                     (assoc ::release-flag release-flag)))))
+   (let [build-config (shadow-config/get-build! :app)]
+    (spit "public/index.html" (index-html build-config))
+    (shadow/watch (-> build-config
+                      (assoc ::release-flag release-flag))))))
 
 ;;;;;;;;;;;;;;;;;;;
 ;; Reading environment variables
